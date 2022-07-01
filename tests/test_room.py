30be9adbdd1fe8664ed17e2c85767013702d2ca6
Nathaniel Forsyth <ironsideforsyth@gmail.com>
2022-07-01 12:03:50 +0100

    added tests for room guests and songs

--- tests/test_room.py
+++ tests/test_room.py
@@ -5,5 +5,9 @@ class TestRoom(unittest.TestCase):
     def setUp(self):
         self.room1 = Room("The Freddie Mercury Room")
     
-    def test_roo_has_name(self):
+    def test_room_has_name(self):
         self.assertEqual("The Freddie Mercury Room", self.room1.name)
+    def test_room_has_guests(self):
+        self.assertEqual(0, len(self.room1.guests))
+    def test_room_has_songs(self):
+        self.assertEqual(0, len(self.room1.songs))
