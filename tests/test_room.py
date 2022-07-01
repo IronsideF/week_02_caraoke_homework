93c622aaeb0deda521d98fc6dd4fa23f7af68e2c
Nathaniel Forsyth <ironsideforsyth@gmail.com>
2022-07-01 12:02:07 +0100

    basic properties set up and tested

--- tests/test_room.py
+++ tests/test_room.py
@@ -2,4 +2,8 @@ import unittest
 from classes.room import Room
 
 class TestRoom(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.room1 = Room("The Freddie Mercury Room")
+    
+    def test_roo_has_name(self):
+        self.assertEqual("The Freddie Mercury Room", self.room1.name)
